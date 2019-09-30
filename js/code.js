d86a619e8ef2a444b09162a0245143bfb12215eb
thanhtan2810 <53827504+thanhtan2810@users.noreply.github.com>
2019-09-30 16:53:55 +0700

    javascripcoban

---
+++ js/code.js
@@ -0,0 +1,31 @@
+// var biencuatoi="xin chào các bạn"
+// alert(biencuatoi);
+// var bien2=5; //kiểu dữ liện dạng số;
+// var bien3='Xin chào 2'// kiểu dữ liệu object;
+// var a=5;
+// var b=6;
+// var c=(a*b)/2;
+// if(a>c&&b>6){
+// alert("A lớn hơn c và b lớn hơn 6");
+// }else{
+//     alert("bài toán sai")
+// }
+//tính tổng các số tự nhiên từ 1 đến 100
+//}
+ 
+    var i=1;
+    var tong=1;
+    // for(i=1;i<=100;i++){
+    //     tong+=i;
+    // }
+    // alert(tong);
+//  while(i<=100){
+//      tong+=i;
+//      i++;
+//  }
+//  alert(tong);
+do{
+    tong+=i;
+    i++;
+}while(i<=100);
+alert(tong);
